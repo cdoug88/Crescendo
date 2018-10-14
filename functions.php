fca8ee7817970d48408f6e39cc402139b55babd8
cdoug88 <chase@gracefellowship.net>
2018-10-14 17:29:11 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 add_action( 'admin_menu', 'linked_url' );
     function linked_url() {
-    add_menu_page( 'linked_url', 'External link', 'read', 'my_slug', '', 'dashicons-text', 1 );
+    add_menu_page( 'linked_url', 'External link', 'read', 'my_slug', '', 'dashicons-text', 9999 );
     }
 
     add_action( 'admin_menu' , 'linkedurl_function' );
     function linkedurl_function() {
     global $menu;
-    $menu[1][2] = "http://www.example.com";
+    $menu[1][2] = "http://citylinecreative.com";
     }
 
 
