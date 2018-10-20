6ebf911b0dfcb5a2150bd9b1e5563b2f582da9df
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-20 01:19:01 -0500

    no message

--- subpage.php
+++ subpage.php
@@ -6,6 +6,7 @@ if ( has_post_thumbnail() ) {
 	the_post_thumbnail_url();
 } 
 ?>');">
+</div>
 <section id="content" role="main">
 	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
