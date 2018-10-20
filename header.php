a3f03e062b3a79e5bb89c523e15350e7e1f875c8
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-20 01:27:15 -0500

    no message

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 		<?php wp_head(); ?>
 	</head> 
+			  <div class="hero wide" style="background: url('<?php 
+if ( has_post_thumbnail() ) { 
+	the_post_thumbnail_url();
+} 
+?>');background-position: center center;width: 100%; height: 40vh; background-size: cover;">
 	<header id="primary" class="primary wide">
 	  <div class="container">
 	    <div id="logo">
@@ -27,13 +32,10 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 		</nav>
 	  </div>
-	  <div class="hero wide" style="background: url('<?php 
-if ( has_post_thumbnail() ) { 
-	the_post_thumbnail_url();
-} 
-?>')">
-	  </div>
-	</header>	
+
+	</header>
+
+	  </div>	
 		
 				
 
