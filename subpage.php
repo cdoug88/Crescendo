b193b203145f467a26104cb35a093dfce0e5e681
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-20 01:08:12 -0500

    no message

--- subpage.php
+++ subpage.php
@@ -1,7 +1,7 @@
 <?php /* Template Name: PageWithoutSidebar */ ?>
 
 <?php get_header(); ?>
-<div class="hero wide" style="background: url('<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>');">
+<div class="hero wide" style="background: url(<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>);">
 <section id="content" role="main">
 	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
