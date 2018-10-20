c7d37dbb9e21de0046e12a96f92e2e28f5abb4d8
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-20 00:54:52 -0500

    no message

--- subpage.php
+++ subpage.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: PageWithoutSidebar */ ?>
+<?php /* Template Name: Subpage */ ?>
 
 	<?php get_header(); ?>
   <body <?php body_class(); ?>>
@@ -9,13 +9,9 @@
         </div>
 
       </div>
-		<section id="content" role="main">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'entry' ); ?>
-			<?php comments_template(); ?>
-			<?php endwhile; endif; ?>
-			<?php get_template_part( 'nav', 'below' ); ?>
-		</section>
+
+		<?php the_content(); ?>
+		
       <!--- ADD HOME PAGE SECTIONS HERE --->
 	  <?php get_footer(); ?>
     </div>
