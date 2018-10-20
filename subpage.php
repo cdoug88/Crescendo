e40e598a4fc3c8051327773c1261f6698d2acfa0
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-20 00:58:21 -0500

    no message

--- subpage.php
+++ subpage.php
@@ -11,7 +11,7 @@
       </div>
 		<section id="content" role="main">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'entry' ); ?>
+
 			<?php endwhile; endif; ?>
 			
 		</section>
