716af3fc5bf649765c327240fc34558cebc43856
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-20 11:31:17 -0500

    no message

--- subpage.php
+++ subpage.php
@@ -3,23 +3,23 @@
 <?php get_header(); ?>
 
 <section id="content" role="main">
-	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="header">
-				<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-			</header>
-			<section class="entry-content">
-				
-				<?php the_content(); ?>
-				<div class="entry-links">
-					<?php wp_link_pages(); ?>
-				</div>
-			</section>
-		</article>
-<!-- 	<?php if ( ! post_password_required() ) comments_template( '', true ); ?> -->
-	<?php endwhile; endif; ?>
-	
+	<div class="container">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="header">
+					<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+				</header>
+				<section class="entry-content">
+					
+					<?php the_content(); ?>
+					<div class="entry-links">
+						<?php wp_link_pages(); ?>
+					</div>
+				</section>
+			</article>
+	<!-- 	<?php if ( ! post_password_required() ) comments_template( '', true ); ?> -->
+		<?php endwhile; endif; ?>
+	</div>
 </section>
 <?php get_footer(); ?>
 
