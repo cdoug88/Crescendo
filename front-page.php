2cc026d610baad0b8597fe35cc95464db1974c20
Chase Douglas <chase.douglas@artistrylabs.com>
2018-10-21 00:14:25 -0500

    no message

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 <?php get_header(); ?>
 
 <section id="content" role="main">
-	<div class="container">
+	
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="header">
@@ -10,7 +10,7 @@
 				</header>
 				<section class="entry-content">
 					
-					<?php the_content(); ?>
+					
 					<div class="entry-links">
 						<?php wp_link_pages(); ?>
 					</div>
@@ -18,6 +18,8 @@
 			</article>
 	<!-- 	<?php if ( ! post_password_required() ) comments_template( '', true ); ?> -->
 		<?php endwhile; endif; ?>
-	</div>
+
 </section>
+<div class="black-bar">
+</div>
 <?php get_footer(); ?>
