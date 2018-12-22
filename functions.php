4a34a30ed2ec6b2eedb0393ea86af2180d20d330
Chase Douglas <chase@citylinecreative.com>
2018-12-21 20:58:27 -0600

    updated function with new plugin name

--- functions.php
+++ functions.php
@@ -34,8 +34,8 @@ function my_theme_register_required_plugins() {
 		),
 		array(
 			'name'               => 'Grid Builder', // The plugin name.
-			'slug'               => 'bs3-grid-builder', // The plugin slug (typically the folder name).
-			'source'             => get_stylesheet_directory() . '/inc/bs3-grid-builder.zip', // The plugin source.
+			'slug'               => 'bs3-grid-builder1', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/inc/bs3-grid-builder1.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
